6dde2e76014750dd76be5d38eaaba530b71804c4
stanislavstashchuk <stashchuk_stanislav@ukr.net>
2021-06-17 23:59:14 +0300

    initial

--- js/scriptNine.js
+++ js/scriptNine.js
@@ -4,4 +4,10 @@ let thirdNumber = (number - number % 100)/100;
 let secondNumber = (number - number % 10)/10 - (thirdNumber * 10);
 let firstNumber = number - (thirdNumber * 100) - (secondNumber * 10);
 
-alert(String(firstNumber)+String(secondNumber)+String(thirdNumber));
+if (firstNumber !=0) {
+    alert(String(firstNumber)+String(secondNumber)+String(thirdNumber));
+} else if (firstNumber== 0 && secondNumber !=0) {
+    alert(String(secondNumber)+String(thirdNumber));
+} else if (firstNumber==0 && secondNumber==0){
+    alert(String(thirdNumber));
+}
